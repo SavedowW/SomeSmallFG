213896a0b6e9cad7f749b579e9971af2ae9cf8e0
DeadM00n <velikanov-em@yandex.ru>
2024-03-23 14:25:01 +0300

    Added state marker for actions

---
+++ src/Core/StateMarker.cpp
@@ -0,0 +1,42 @@
+#include "StateMarker.h"
+
+StateMarker::StateMarker(int count_) :
+    m_stateMarks(count_ / STATE_HOLDER_SIZE + 1 , 0)
+{
+
+}
+
+StateMarker::StateMarker(const StateMarker &sm_)
+{
+    m_stateMarks = sm_.m_stateMarks;
+}
+
+StateMarker::StateMarker(StateMarker &&sm_)
+{
+    m_stateMarks = std::move(sm_.m_stateMarks);
+}
+
+StateMarker::StateMarker(int count_, const std::vector<int> &trueFields_) :
+    m_stateMarks(count_ / STATE_HOLDER_SIZE + 1 , 0)
+{
+    for (const auto &el: trueFields_)
+    {
+        auto arrid = el / STATE_HOLDER_SIZE;
+        auto bitid = el % STATE_HOLDER_SIZE;
+        m_stateMarks[arrid] = m_stateMarks[arrid] | ((StateHolder_t)1 << bitid);
+    }
+}
+
+void StateMarker::toggleMark(int id_)
+{
+    auto arrid = id_ / STATE_HOLDER_SIZE;
+    auto bitid = id_ % STATE_HOLDER_SIZE;
+    m_stateMarks[arrid] = m_stateMarks[arrid] ^ ((StateHolder_t)1 << bitid);
+}
+
+bool StateMarker::getMark(int id_) const
+{
+    auto arrid = id_ / STATE_HOLDER_SIZE;
+    auto bitid = id_ % STATE_HOLDER_SIZE;
+    return ((m_stateMarks[arrid] >> bitid) & (StateHolder_t)1);
+}
---
+++ src/Core/StateMarker.h
@@ -0,0 +1,29 @@
+#ifndef STATE_MARKER_H_
+#define STATE_MARKER_H_
+
+#include <iostream>
+#include <array>
+#include <chrono>
+
+using StateHolder_t = uint64_t;
+constexpr inline int STATE_HOLDER_SIZE = sizeof(StateHolder_t) * 8;
+
+// Hold count_ bool values set to false by default
+// For some reason works notably faster than just a regular std::vector<bool> with compiler optimization
+// which should do the same thing under the hood
+class StateMarker
+{
+public:
+    StateMarker(int count_);
+    StateMarker(const StateMarker &sm_);
+    StateMarker(StateMarker &&sm_);
+    StateMarker(int count_, const std::vector<int> &trueFields_);
+    void toggleMark(int id_);
+    bool getMark(int id_) const;
+
+private:
+    std::vector<StateHolder_t> m_stateMarks;
+
+};
+
+#endif
